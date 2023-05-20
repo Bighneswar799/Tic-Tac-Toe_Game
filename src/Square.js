d0b1fcb1396f1dee227b77abbe152da8b364fcb1
Bighneswar Mahapatra <123478215+Bighneswar799@users.noreply.github.com>
2023-05-20 12:42:32 +0530

    Add files via upload

---
+++ src/Square.js
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import './Square.css'
+
+function Square({value, onClick}) {
+  return (
+    <>
+      <button 
+        className="square"
+        onClick={onClick}>
+        {value}
+      </button>
+    </>
+  )
+}
+
+function Board () {
+
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [isX, setIsX] = useState(true)
+  const handleClick = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = isX ? 'X' : '0'
+    setSquares(squares)
+    setIsX(!isX)  
+  }
+
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = `Winner: ${winner}`
+    } else { 
+      status = `Next Player: ${isX ? 'X' : '0'}`
+    }
+
+    const handleRestart = () => {
+      setIsX(true)
+      setSquares(Array(9).fill(null))
+    }
+
+    const renderSquare = (i) => {
+      return <Square value = {squares[i]} onClick={() => handleClick(i)} />
+    }
+
+  return (
+    <div className="board">
+      <h2><u>Tic-Tac-Toe</u></h2>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <div className="status">{status}</div>
+      <button className="restart" onClick={handleRestart}>Restart Game!</button>
+    </div>
+  )
+}
+
+function calculateWinner (squares) {
+  const winnerPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < winnerPatterns.length; i++) {
+    const [a, b, c] = winnerPatterns[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+}
+
+export  {Square, Board}
